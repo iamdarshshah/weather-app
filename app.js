4f4a0cc62e3d6c43d44570e9198093e5c313d7b4
iamdarshshah <imdarshshah@gmail.com>
2020-06-29 22:37:36 +0530

    Chaining callbacks together.

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-// const dotenv = require("dotenv");
-// const yargs = require("yargs");
+const dotenv = require("dotenv");
+const yargs = require("yargs");
 
-// const geocode = require("./geocode/geocode");
-// dotenv.config();
+const geocode = require("./geocode/geocode");
+const weather = require("./weather/weather");
+dotenv.config();
 
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: "address",
-//       describe: "Address to fetch weather for",
-//       string: true,
-//     },
-//   })
-//   .help()
-//   .alias("help", "h").argv;
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true,
+    },
+  })
+  .help()
+  .alias("help", "h").argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
-const request = require("request");
-
-request(
-  {
-    url:
-      "https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/39.9396284,-75.186395999999",
-    json: true,
-  },
-  (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      console.log(body.currently.temprature);
-    } else {
-      console.log("Unable to fetch weather.");
-    }
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(
+      results.latitude,
+      results.longitude,
+      (errorMessage, weatherResults) => {
+        if (errorMessage) {
+          console.log(errorMessage);
+        } else {
+          console.log(
+            `It's currently ${weatherResults.temprature}. It feels like ${weatherResults.apparentTemperature}.`
+          );
+        }
+      }
+    );
   }
-);
+});
---
+++ weather/weather.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+var getWeather = (lat, lng, callback) => {
+  request(
+    {
+      url: `https://api.forecast.io/forecast/${process.env.API_KEY_FORECAST}/${lat},${lng}`,
+      json: true,
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        callback(undefined, {
+          temperature: body.currently.temprature,
+          apparentTemperature: body.currently.apparentTemperature,
+        });
+      } else {
+        callback("Unable to fetch weather.");
+      }
+    }
+  );
+};
+
+module.exports.getWeather = getWeather;
