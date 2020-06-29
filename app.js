62b7f1b7e3c45c395e41337eaa9b268d259b5990
iamdarshshah <imdarshshah@gmail.com>
2020-06-29 18:18:29 +0530

    Pulled off the data we need to request the weather api

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ request(
     json: true,
   },
   (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Address: ${body.results[0].geometry.location.lat}`);
+    console.log(`Address: ${body.results[0].geometry.location.lng}`);
   }
 );
