f1240fc9a1d3458710b22e4a3ca11f21b8547d22
iamdarshshah <imdarshshah@gmail.com>
2020-06-29 18:30:17 +0530

    Encoding User Input

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const request = require("request");
 const dotenv = require("dotenv");
+const yargs = require("yargs");
 dotenv.config();
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true,
+    },
+  })
+  .help()
+  .alias("help", "h").argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=1301%20lombard%20street%20philadelphia`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=${encodedAddress}`,
     json: true,
   },
   (error, response, body) => {
