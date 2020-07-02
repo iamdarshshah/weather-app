2d26d872dad428425822a4bc9115345b84e10286
iamdarshshah <imdarshshah@gmail.com>
2020-07-02 23:33:57 +0530

    async add with promise

--- playground/promise.js
+++ playground/promise.js
@@ -1,15 +1,36 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve("Hey, It Worked.");
-    reject("Unable to fulfill promise");
-  }, 2500);
-});
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === "number" && typeof b === "number") {
+        resolve(a + b);
+      } else {
+        reject("Arguments must be numbers.");
+      }
+    }, 1500);
+  });
+};
 
-somePromise.then(
-  (message) => {
-    console.log("Success: ", message);
+asyncAdd(9, 7).then(
+  (response) => {
+    console.log(response);
   },
   (errorMessage) => {
-    console.log("Error: ", errorMessage);
+    console.log(errorMessage);
   }
 );
+
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve("Hey, It Worked.");
+//     reject("Unable to fulfill promise");
+//   }, 2500);
+// });
+
+// somePromise.then(
+//   (message) => {
+//     console.log("Success: ", message);
+//   },
+//   (errorMessage) => {
+//     console.log("Error: ", errorMessage);
+//   }
+// );
